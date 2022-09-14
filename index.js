53be34bcb0aea5eea10953d7b26e423a3311d348
G Codes <graciela.mejia1@yahoo.com>
2022-09-14 09:38:08 -0700

    add promptuser for name, id and email

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-module.exports = function () {};
+const inquirer = require("inquirer");
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the employee's name? (Required)",
+      validate: (name) => {
+        if (name) {
+          return true;
+        } else {
+          console.log("Please enter employee's name.");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the employee's id?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the employee's email?",
+      validate: (email) => {
+        if (email) {
+          return true;
+        } else {
+          console.log("Please enter employee's email.");
+        }
+      },
+    },
+  ]);
+};
+
+const promptEngineer = () => {
+  console.log(`
+  =================
+  Add a Engineer.
+  =================
+  `);
+  //add engineer info
+};
+
+promptUser();
+
+promptEngineer();
