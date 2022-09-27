a4ed6f5dd5f9792095caa594f1d37dc92f16427a
G Codes <graciela.mejia1@yahoo.com>
2022-09-26 22:16:12 -0700

    fix typo in pageTemplate

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -1,8 +1,3 @@
-const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-const Manager = require("../lib/Manager");
-
 const pageTemplate = (teamArray) => {
   return `
   <!DOCTYPE html>
